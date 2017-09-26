2ef519284a4fedac824e2b60f488f81e46cf35c5
lokiloko <ianw.tkj2@gmail.com>
2017-09-26 11:15:46 +0700

    Release 1

--- composition.js
+++ composition.js
@@ -1,9 +1,9 @@
 "use strict"
 const fs = require('fs');
 class Cookie {
-  constructor() {
+  constructor(ingredients) {
     this.status = "mentah";
-    this.ingredients = [];
+    this.ingredients = ingredients;
   }
   bake() {
     this.status = "selesai dimasak"
@@ -11,50 +11,94 @@ class Cookie {
 }
 
 class PeanutButter extends Cookie {
-  constructor() {
-    super()
+  constructor(ingredients) {
+    super(ingredients)
+    this.name = "Peanut Butter";
     this.peanut_count = 100;
   }
 }
 
 class ChocolateChip extends Cookie {
-  constructor() {
-    super()
+  constructor(ingredients) {
+    super(ingredients)
+    this.name = "Chocolate Chip"
     this.choc_chip_count = 200;
   }
 }
 
 class OtherCookie extends Cookie {
-  constructor(){
-    super()
+  constructor(ingredients,name) {
+    super(ingredients)
+    this.name = name;
     this.other_count = 150;
   }
 }
 
 class CookieFactory {
-  constructor(){
-
-  }
   static create(options) {
     var data = fs.readFileSync(options).toString().split('\n');
-    var cookies = []
+    var cookies = [];
     for (var i = 0; i < data.length - 1; i++) {
-      if(data[i].indexOf('peanut') > -1){
-        var obj = new PeanutButter();
-        cookies.push(obj);
+      var ingredients = [];
+      var dataKue = data[i].split('=');
+      var dataIngredients = dataKue[1].split(',')
+      for (var j = 0; j < dataIngredients.length; j++) {
+        var temp = dataIngredients[j].split(':')
+        var sugar = false;
+        if(temp[1].indexOf('sugar') > -1)
+          sugar = true;
+
+        var options = {
+          name : temp[1],
+          amount : temp[0],
+          has_sugar : sugar,
+        }
+        var objIngredients = new Ingredients(options);
+        ingredients.push(objIngredients);
       }
-      else if(data[i].indexOf('chip') > -1){
-        var obj = new ChocolateChip();
+      if (dataKue[0].indexOf('peanut') > -1) {
+        var obj = new PeanutButter(ingredients);
+        cookies.push(obj)
+      } else if (dataKue[0].indexOf('chip') > -1) {
+        var obj = new ChocolateChip(ingredients);
         cookies.push(obj);
-      }
-      else{
-        var obj = new OtherCookie();
+      } else {
+        var obj = new OtherCookie(ingredients,dataKue[0]);
         cookies.push(obj);
       }
     }
     return cookies;
   }
+  static cookieRecomendation(day, batch_of_cookies){
+    var sugarFreeFoods = [];
+    for(var i =0;i<batch_of_cookies.length;i++){
+      var sugar = [];
+      for(var j = 0;j<batch_of_cookies[i].ingredients.length;j++){
+        if(batch_of_cookies[i].ingredients[j].has_sugar === true){
+          sugar.push(batch_of_cookies[i].ingredients[j].name);
+        }
+      }
+      if(sugar.length === 0){
+        sugarFreeFoods.push(batch_of_cookies[i]);
+      }
+    }
+    return sugarFreeFoods;
+  }
 }
-var options = 'cookies.txt'
+
+class Ingredients {
+  constructor(options) {
+    this.name = options['name'];
+    this.amount = options['amount'];
+    this.has_sugar = options['has_sugar'];
+  }
+}
+var options = 'cookies.txt';
 let batch_of_cookies = CookieFactory.create(options);
 console.log(batch_of_cookies);
+
+let sugarFreeFoods = CookieFactory.cookieRecomendation("tuesday", batch_of_cookies);
+console.log("sugar free cakes are :");
+for(let i =0;i<sugarFreeFoods.length;i++){
+  console.log(sugarFreeFoods[i].name);
+}
