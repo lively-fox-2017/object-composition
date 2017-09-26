3390696aa9dbebc14e23f76a1b3bad7761da2507
setiadijoe <setiadi_y56@yahoo.co.id>
2017-09-26 11:10:49 +0700

    static Cookie Factory berjalan

--- composition.js
+++ composition.js
@@ -3,10 +3,12 @@
 const fs = require('fs');
 var cookies = fs.readFileSync('cookies.txt', 'UTF-8')
 var options = cookies.split('\n')
+
 class Cookie {
     constructor(name){
         this._name = name
         this.status = "mentah"
+        this.ingredients = []
     }
 
     bake() {
@@ -21,11 +23,39 @@ class PeanutButter extends Cookie {
     }
 }
 
-class ChocholateChip extends Cookie {
+class ChocolateChip extends Cookie {
     constructor(name){
         super(name)
         this.choc_chip_count = 200
     }
 }
 
-console.log(typeof options)
+class OtherCookie extends Cookie {
+    constructor(name){
+        super(name)
+        this.other_cookie = 150
+    }
+}
+
+class CookieFactory {
+    static create(option){
+        this.cookies = []
+        for (var i = 0; i < option.length - 1; i++) {
+            if (option[i] === 'peanut butter') {
+                let cookie = new PeanutButter(option[i])
+                this.cookies.push(cookie)
+            } else if (option[i] === 'chocolate chip') {
+                let cookie = new ChocolateChip(option[i])
+                this.cookies.push(cookie)
+            } else {
+                let cookie = new OtherCookie(option[i])
+                this.cookies.push(cookie)
+            }
+        }
+        return this.cookies
+    }
+}
+
+var cookie = CookieFactory.create(options)
+
+console.log(cookie)
