7137ef87b088768d0f3394e73e8e5aa5a2b490a7
munawirali <munawirali@gmail.com>
2017-09-26 19:52:56 +0700

    done munawirali

--- composition.js
+++ composition.js
@@ -6,45 +6,44 @@ const fs = require('fs');
 
 class Ingredient {
   constructor(options) {
-    this.name=options[0]; //console.log('-------------------------------------'+options[1]);
-    this.amount=options[1].trim();
-    // if (options[1].toUpperCase()==' SUGAR') {
-    //   this.has_sugar=true;
-    // } else this.has_sugar=false;
-    // console.log(options);
+    this.name=options[1];
+    this.amount=options[0].trim();
   }
 }
 
 class Cookie {
   constructor(nama) {
     this.status='mentah';
-    this.ingredient = this.getIngredient(nama); //console.log(this.ingredient);
+    this.ingredient = this.getIngredient(nama);
     this.has_sugar = this.hasSugar(this.ingredient);
   }
   getIngredient(nama){ //console.log('nama='+nama+'================'+ingred.length);
     let bhn_per_cookie=[];
+    // console.log(nama);
     // console.log(ingred);
     for (var i = 0; i < ingred.length; i++) {
-      //console.log('-------------'+ingred[i][0]+'== '+nama);
-      if (ingred[i][0].trim()==nama) {
+      // console.log('-------------'+ingred[i][0]+'== '+nama);
+      if (ingred[i][0].trim()==nama.trim()) {
+        // console.log('-----------===',ingred[i][1].split(','));
         bhn_per_cookie.push(ingred[i][1].split(','));
       }
     }
-    //console.log('bahan-bahan='+bhn_per_cookie[0].length);
+    // console.log('bahan-bahan='+bhn_per_cookie[0].length);
     let bhn_bhn=[]; //satu baris
     for (var i = 0; i < bhn_per_cookie[0].length; i++) {//console.log(bhn_per_cookie[i]);
       let newIngred = new Ingredient(bhn_per_cookie[0][i].split(':'))
       bhn_bhn.push(newIngred);
     }
+    // console.log(bhn_bhn);
     return bhn_bhn; //console.log('bahan-bahan'+bhn_bhn);
   }
 
   hasSugar(arr){
-    // console.log(arr);//.amount);
+    // console.log('-------------',arr);//.amount);
     for (var i = 0; i < arr.length; i++) {
       // console.log(arr[i]);
       // console.log('-----------------'+arr[i].amount.trim());
-      if (arr[i].amount.indexOf('sugar')>-1) {
+      if (arr[i].name.indexOf('sugar')>-1) {
         // console.log(arr[i].amount+'------------------------');
         return true;
       }
@@ -80,19 +79,19 @@ class OtherCookis extends Cookie{
 class CookieFactory {
   static create (options){
     this.cookies=[]
-    for (var i = 0; i < options.length-1; i++) {
-      if (options[i]=='peanut butter') {
+    for (var i = 0; i < options.length; i++) {
+      if (options[i].trim()=='peanut butter') {
         let obj = new PeanutButter(options[i])
-        obj.name=options[i];
+        obj.name=options[i].trim();
         this.cookies.push(obj);
       } else
-      if (options[i]=='chocolate chip') {
+      if (options[i].trim()=='chocolate chip') {
         let obj = new ChocolateChip(options[i])
-        obj.name=options[i];
+        obj.name=options[i].trim();
         this.cookies.push(obj);
       } else {
         let obj = new OtherCookis(options[i])
-        obj.name=options[i];
+        obj.name=options[i].trim();
         this.cookies.push(obj);
       }
     }
@@ -111,22 +110,23 @@ class CookieFactory {
   }
 }
 
-let options=fs.readFileSync('./cookies.txt','UTF-8').toString().split(/\n/);
-let ingred_temp=fs.readFileSync('./ingredient.txt','UTF-8').toString().split(/\n/);//.splice(0,4);
-//let ingred_temp2=ingred_temp[1].split('=');
+let ingred_temp=fs.readFileSync('./cookies.txt','UTF-8').toString().split(/\n/);
+let options=[];
 let ingred=[];
 for (var i = 0; i < ingred_temp.length-1; i++) {
   ingred.push(ingred_temp[i].split('='));
+  options.push(ingred[i][0]);
 }
-//console.log(ingred);
-// console.log(ingred_temp2);
+// console.log(ingred);
+// console.log(options);
 
 let batch_of_cookies = CookieFactory.create(options);
-console.log(batch_of_cookies[0].ingredient);
 console.log(batch_of_cookies);
+console.log(batch_of_cookies[0].ingredient);
 
 let sugarFreeFoods = CookieFactory.cookieRecomendation('tuesday',batch_of_cookies);
 console.log('sugar free cakes are:');
 for (var i = 0; i < sugarFreeFoods.length; i++) {
   console.log(sugarFreeFoods[i].name);
+  console.log(sugarFreeFoods[i]);
 }
