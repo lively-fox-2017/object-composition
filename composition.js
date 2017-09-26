08ca5bd6bd3ff25c0d6adbab86b9dc2dbc947140
aditReza <lete.note3@gmail.com>
2017-09-26 19:39:32 +0700

    release 1 : add ingredients

--- composition.js
+++ composition.js
@@ -6,12 +6,22 @@ let dataFixed = []
 for(let idx=0; idx<importFile.length; idx++){
   dataFixed.push(importFile[idx].split('='))
 }
+//console.log(dataFixed[0][1])
 
+class Ingredient{
+  constructor(options){
+    this.name = options['name']
+    this.amount = options['amount']
+    this.has_sugar = options['has_sugar']
+  }
+  // your method here if needed
+  //console.log(this.name)
+}
 class Cookie{
-  constructor(name){
+  constructor(name, composition){
     this.name = name
     this.status = 'mentah'
-    this.ingredients = []
+    this.ingredients = composition
   }
 
   bake(){
@@ -20,43 +30,51 @@ class Cookie{
 }
 
 class PeanutButter extends Cookie {
-  constructor(name){
-    super(name)
+  constructor(name, composition){
+    super(name, composition)
     this.peanut_count = 100
+
   }
 }
 
 class ChocholateChip extends Cookie{
-  constructor(name){
-    super(name)
+  constructor(name, composition){
+    super(name, composition)
     this.choc_chip_count = 200
+
   }
 }
 
 class OtherCookies extends Cookie{
-  constructor(name){
-    super(name)
+  constructor(name, composition){
+    super(name, composition)
     this.other_count = 150
+
   }
 }
 
 
 class CookieFactory{
-  static create(options){
+  static create(options, composition){
     // accepts a list of cookie types and returns those cookies
+
     let result = []
     for(let i=0; i<importFile.length-1; i++){
+      //console.log(dataFixed[i][1])
       if(dataFixed[i][0].trim() === 'peanut butter'){
-        result.push(new PeanutButter(dataFixed[0][0]))
+        result.push(new PeanutButter(dataFixed[0][0], dataFixed[0][1]))
       }else if(dataFixed[i][0].trim() === 'chocolate chip'){
-        result.push(new ChocholateChip(dataFixed[1][0]))
+        result.push(new ChocholateChip(dataFixed[1][0], dataFixed[1][1]))
       }else{
-        result.push(new OtherCookies(dataFixed[i][0]))
+        result.push(new OtherCookies(dataFixed[i][0], dataFixed[i][1]))
       }
     }
     return result
+
+
   }
   // define other methods as needed
+
 }
 
 // contoh driver code
@@ -67,3 +85,9 @@ class CookieFactory{
 
 let batch_of_cookies = CookieFactory.create('cookies.txt')
 console.log(batch_of_cookies)
+
+//let sugarFreeFoods = CookieFactory.cookieRecommendation('tuesday', batch_of_cookies)
+// console.log('sugar free cakes are :')
+// for(let i=0; i<sugarFreeFoods.length; i++){
+//   console.log(sugarFreeFoods[i].name)
+// }
