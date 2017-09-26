f771fb13d4301468cdb8dcb5fa2f46ad47f65e6f
aditReza <lete.note3@gmail.com>
2017-09-26 18:04:22 +0700

    release 1 : refaktor code

--- composition.js
+++ composition.js
@@ -7,7 +7,6 @@ for(let idx=0; idx<importFile.length; idx++){
   dataFixed.push(importFile[idx].split('='))
 }
 
-
 class Cookie{
   constructor(name){
     this.name = name
@@ -45,16 +44,14 @@ class OtherCookies extends Cookie{
 class CookieFactory{
   static create(options){
     // accepts a list of cookie types and returns those cookies
-
     let result = []
     for(let i=0; i<importFile.length-1; i++){
-      //console.log(importFile[i])
-      if(importFile[i] === 'peanut butter'){
-        result.push(new PeanutButter(importFile[0]))
-      }else if(importFile[i] === 'chocolate chip'){
-        result.push(new ChocholateChip(importFile[1]))
+      if(dataFixed[i][0].trim() === 'peanut butter'){
+        result.push(new PeanutButter(dataFixed[0][0]))
+      }else if(dataFixed[i][0].trim() === 'chocolate chip'){
+        result.push(new ChocholateChip(dataFixed[1][0]))
       }else{
-        result.push(new OtherCookies(importFile[i]))
+        result.push(new OtherCookies(dataFixed[i][0]))
       }
     }
     return result
