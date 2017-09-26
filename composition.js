b024ef9390757671cc95c2799de15501c27b5bb4
setiadijoe <setiadi_y56@yahoo.co.id>
2017-09-26 15:42:16 +0700

    ingredients sudah didapat

--- composition.js
+++ composition.js
@@ -3,12 +3,26 @@
 const fs = require('fs');
 var cookies = fs.readFileSync('cookies.txt', 'UTF-8')
 var options = cookies.split('\n')
+var bahan = fs.readFileSync('ingredients.txt', 'UTF-8').split('\n')
+let ingred = []
+for (let i = 0; i < bahan.length; i++) {
+    ingred[i] = bahan[i].split('=');
+    ingred[i][1] = ingred[i][1].split(',')
+}
 
 class Cookie {
-    constructor(name){
+    constructor(name, options){
         this._name = name
         this.status = "mentah"
-        this.ingredients = []
+        this.ingredients = this.getIngredients(options)
+    }
+
+    getIngredients(options){
+        let ingredient = []
+        for (let i = 0; i < options.length; i++) {
+            ingredient.push(new Ingredients(options[i].split(':')))
+        }
+        return ingredient
     }
 
     bake() {
@@ -17,45 +31,60 @@ class Cookie {
 }
 
 class PeanutButter extends Cookie {
-    constructor(name){
-        super(name)
+    constructor(name, options){
+        super(name, options)
         this.peanut_count = 100
     }
 }
 
 class ChocolateChip extends Cookie {
-    constructor(name){
-        super(name)
+    constructor(name, options){
+        super(name, options)
         this.choc_chip_count = 200
     }
 }
 
 class OtherCookie extends Cookie {
-    constructor(name){
-        super(name)
+    constructor(name, options){
+        super(name, options)
         this.other_cookie = 150
     }
 }
 
+// Release 1 menambahkan bahan-bahan cookienya ingredients
+
+class Ingredients {
+    constructor(ingred){
+        this.amount = ingred[0]
+        this.receipt = ingred[1]
+    }
+}
+
+// Release 0 bikin cookie factory dengan kondisi kue masih mentah semuah
 class CookieFactory {
-    static create(option){
+    static create(option, ingred){
         this.cookies = []
-        for (var i = 0; i < option.length - 1; i++) {
+        for (let i = 0; i < option.length; i++) {
             if (option[i] === 'peanut butter') {
-                let cookie = new PeanutButter(option[i])
+                let cookie = new PeanutButter(option[i],ingred[i][1])
                 this.cookies.push(cookie)
             } else if (option[i] === 'chocolate chip') {
-                let cookie = new ChocolateChip(option[i])
+                let cookie = new ChocolateChip(option[i], ingred[i][1])
                 this.cookies.push(cookie)
             } else {
-                let cookie = new OtherCookie(option[i])
+                let cookie = new OtherCookie(option[i], ingred[i][1])
                 this.cookies.push(cookie)
             }
         }
         return this.cookies
     }
+
 }
 
-var cookie = CookieFactory.create(options)
 
-console.log(cookie)
+
+
+var cookie = CookieFactory.create(options, ingred)
+
+console.log(cookie[0])
+// console.log(bahan)
